094fec67110b571e27d91c3ecfd42a1a73f188bc
Tiana Tran <tiana.tran01@student.csulb.edu>
2025-10-25 13:32:27 -0700

    activity button

--- marina_hacks/src/App.jsx
+++ marina_hacks/src/App.jsx
@@ -1,9 +1,18 @@
-import Activities from './activities'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PastelButtons from './PastelButtons';
+import Activities from './activities';
 import './App.css'
 
 function App() {
   return (
-    <Activities />
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<PastelButtons />} />
+          <Route path="/activities" element={<Activities />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
